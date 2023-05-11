a591cd4e5169bf388ff96d8883d1e3936096eeeb
Sahil-107 <messisahil7@gmail.com>
2023-05-11 21:00:33 +0530

    added service

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { SignUp } from '../data-type';
 
 @Component({
   selector: 'app-seller-auth',
@@ -6,7 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent {
-  signup(data:object):void{
-    console.log(data)
+  constructor(private seller:SellerService, private router: Router){}
+
+  signup(data:SignUp):void{
+    // console.log(data)
+    this.seller.userSignUp(data).subscribe((result)=>{
+      if(result){
+        this.router.navigate(['seller-home'])
+      }
+      console.warn(result);
+    });
   }
 }
